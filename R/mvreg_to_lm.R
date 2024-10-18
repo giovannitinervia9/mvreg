ee6c7f718e12be8fe98abd6e6e1ee6abcef5e694
giovannitinervia9 <giovannitinervia9@gmail.com>
2024-10-18 15:21:35 +0200

    changed mvreg_to_lm() to correctly handle data argument in call to lm

--- R/mvreg_to_lm.R
+++ R/mvreg_to_lm.R
@@ -14,8 +14,19 @@ mvreg_to_lm <- function(object) {
   if (!(class(object) %in% c("mvreg", "summary.mvreg"))) {
     stop("Object is not of class 'mvreg' or 'summary.mvreg'.")
   }
+
   formula.mu <- object$call$formula.mu
-  data <- eval(object$call$data, envir = parent.frame())
-  lm_model <- lm(formula.mu, data = data)
+
+
+  if (!is.null(object$call$data)) {
+    data_name <- as.character(object$call$data)
+    data <- eval(object$call$data, envir = parent.frame())
+
+    lm_model <- lm(formula.mu, data = data)
+    lm_model$call$data <- as.name(data_name)
+  } else {
+    lm_model <- lm(formula.mu)
+  }
+
   lm_model
 }
