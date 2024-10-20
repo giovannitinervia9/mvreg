34e3e45370635dfcc7150fd714356a958103f9bf
giovannitinervia9 <giovannitinervia9@gmail.com>
2024-10-20 14:36:02 +0200

    updated documentation for mvreg_start()

--- R/mvreg_start.R
+++ R/mvreg_start.R
@@ -3,12 +3,33 @@
 
 #' Starting values for mvreg
 #'
-#' @param y Response variable vector.
-#' @param x Design matrix for mean component.
-#' @param z Design matrix for variance component.
-#' @param start.s2 A character vector indicating how to select initial values for variance component parameters.
+#' This function computes starting values for the parameters of a heteroskedastic linear model.
+#' The initial estimates for the mean component parameters are calculated using ordinary least squares (OLS),
+#' while the initial estimates for the variance component parameters can be derived from three different methods specified by `start.s2` argument.
 #'
-#' @return Starting values of estimates of parameters
+#' @param y A numeric vector representing the response variable.
+#' @param x A numeric matrix representing the design matrix for the mean component of the model.
+#' @param z A numeric matrix representing the design matrix for the variance component of the model.
+#' @param start.s2 A character string indicating the method for selecting initial values for the variance component parameters.
+#' Options are:
+#' - "residuals": uses the logarithm of the squared residuals from the mean component model to estimate starting values for the variance component (default choice).
+#' - "gamma": uses a Gamma regression on the squared residuals to estimate starting values for the variance component.
+#' - "zero": sets all starting values for the variance component to zero, with the first parameter set to the logarithm of the sample variance of the response variable.
+#'
+#' @return A list containing:
+#' - `start`: A numeric vector of initial parameter estimates, including both mean and variance component parameters.
+#' - `b0`: A numeric vector of initial estimates for the mean component parameters.
+#' - `t0`: A numeric vector of initial estimates for the variance component parameters.
+#'
+#' @details
+#' A first guess for \eqn{\hat{\boldsymbol{\beta}}_0} is obtained using the standard OLS estimator
+#' \eqn{\hat{\boldsymbol{\beta}}_0 = (\mathbf{X}'\mathbf{X})^{-1}\mathbf{X}'\mathbf{y}}.
+#'
+#' A first guess for \eqn{\hat{\boldsymbol{\tau}}_0} is obtained in different ways depending on the specification of the `start.s2` argument:
+#' - If `start.s2` is set to "residuals", the first guess for \eqn{\hat{\boldsymbol{\tau}}_0} is obtained by regressing the logarithm of the squared empirical residuals \eqn{\hat{\varepsilon}_i = y_i - \mathbf{x}_i'\hat{\boldsymbol{\beta}}_0} on the covariates \eqn{\mathbf{Z}}.
+#' - If `start.s2` is set to "gamma", a Gamma regression model is fit with the squared empirical residuals as the response variable and \eqn{\mathbf{Z}} as the covariates.
+#' - If `start.s2` is set to "zero", the first guess is simply \eqn{\hat{\boldsymbol{\tau}}_0 = (\log\hat{S}^2_y, \boldsymbol{0}_{p-1})'} where \eqn{p} is the number of columns of \eqn{\mathbf{Z}}.
+#'
 #' @export
 #'
 #' @importFrom stats Gamma glm.fit
@@ -18,7 +39,7 @@
 #' x2 <- factor(sample(letters[1:3], n, TRUE))
 #' x <- model.matrix(~ x1 + x2)
 #' z1 <- factor(sample(letters[1:3], n, TRUE))
-#' z <- model.matrix(~z1)
+#' z <- model.matrix(~ z1)
 #'
 #' b <- rnorm(ncol(x))
 #' t <- rnorm(ncol(z))
